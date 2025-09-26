b48d93aaee6fbdba31e0ef8b3cc3c840b6394bf2
yayaluft <syanuha66@gmail.com>
2025-09-26 16:38:50 +0700

    user 2

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("hai, ini baris kode pertama");
+        System.out.println("User 2 menambahkan baris baru");
     }
 }
