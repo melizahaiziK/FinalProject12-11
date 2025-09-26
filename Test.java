cb281110018720b88302e79736700e44042b6d5f
melizahaiziK <melizaha1zik@gmail.com>
2025-09-26 16:42:46 +0700

    user 1

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("hai, ini baris kode pertama");
         System.out.println("User 2 menambahkan baris baru");
+        System.out.println("user 1 menambahkan baris baru");
     }
 }
